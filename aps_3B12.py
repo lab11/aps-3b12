0477c1b4b5b90de5b493f338c51b2c93057fbd21
Ye-Sheng Kuo <higherkuo@gmail.com>
2016-04-13 13:21:46 -0400

    restructure everything

--- aps_3B12.py
+++ aps_3B12.py
@@ -1,12 +1,9 @@
 #! /usr/bin/python3
+import APS3B12
+from time import sleep
 
-import serial
-import time
-
-wattage = 0
-inc = 25
-
-load_enabled = 0
+DEV = '/dev/ttyUSB0'
+OFFSET_THRESHOLD = 0.003 # mA
 
 def error_msg():
 	print('Unrecognized command')
@@ -15,128 +12,76 @@ def error_msg():
 	#print('off - deactivate load')
 	#print('exit - quit application')
 	
-def aps_print(text):
-	print(text)
-	ser.write(bytes(text,"utf-8"))
-	time.sleep(.03)
-	
-def setWatt(value):
-	global wattage
-	global load_enabled
-	
-	if load_enabled == 0:
-		print("Load not enabled, command ignored")
-		return
-	
-	wattage	= float(value)
-	print("Setting wattage to " + str(wattage) + " W")
-	print("Please wait for command to finish")
-	#print(wattage)
-	aps_print('MEAS:VOLT?;')
-	voltage = ser.read(8)
-	#print(float(voltage))
-	if(float(voltage) > 0):
-		current = float(value) / float(voltage)
-	else:
-		current = float(value) / 120
-	aps_print('CC:A ' + str(current) + ';')
-	time.sleep(1)
-	aps_print('MEAS:POW?;')
-	power = ser.read(8)
-	#print(float(power))
-	if(float(power) > 0):
-		scale = float(value) / float(power)
-		current = current * scale
-		aps_print('CC:A ' + str(current) + ';')
-	
 def help():
 	print("Commands:")
 	print("on\tTurn load on")
 	print("off\tTurn load off")
+	print("readV\tRead voltage value")
+	print("readI\tRead current value")
+	print("readW\tRead current value")
 	print("watt=XX\tSet load to XX W")
-	print("\tor simply enter a number")
-	print("inc=XX\tSet increment to XX W")
-	print("<enter>\tIncrement wattage by [inc] W")
-	print("\t[inc] is 25 by default")
+	print("amp=XX\tSet load to XX A")
 	print("help\tDisplay help")
-	print("exit\tTurn load off and exit application")
+	print("exit\tTurn load off and exit application\n")
 
-ser = serial.Serial('/dev/com1', 9600)
-
-
-print()
-print("APS 3B12 Control Script")
-print("Connected to: " + ser.name)
-print()
-help()
-print()
-
-aps_print('REM;')
-aps_print('CC:A 0;')
-aps_print('LOAD 0;')
-aps_print('LOC;')
-
-while(1):
-
+def main():
+	print("\nAPS 3B12 Control Script\n")
+	help()
+	myDevice = APS3B12.APS3B12(DEV)
+	#myDevice.get_loadState()
 	# For each command:
 	#	1. Set 3B12 to Remote
 	#	2. Perform operation
 	#	3. Set 3B12 to Local
 
-	textIn = input("> ").split('=')
-	command = textIn[0].strip(' ')
-
-	aps_print('REM;')
-	
-	if len(textIn[0]) == 0:
-		setWatt(wattage + inc)
-	elif len(textIn) == 1:
+	while(1):
+		textIn = input('Enter input: ')
+		textIn = textIn.split('=')
+		command = textIn[0].strip(' ')
 		# No-parameter commands
-		if(command == 'on'):
-			print("Load turning on...")
-			load_enabled = 1
-			aps_print('LOAD 1;')
-		elif(command == 'off'):
-			print("Load turning off...")
-			load_enabled = 0
-			wattage = 0
-			aps_print('CC:A 0;')
-			aps_print('LOAD 0;')
-		elif(command == 'readV'):
-			aps_print('MEAS:VOLT?;')
-			voltage = ser.read(8)
-			print(voltage),
-		elif(command == 'help'):
-			help()
-		elif(command == 'exit'):
-			print("Exiting...")
-			aps_print('CC:A 0;')
-			aps_print('LOAD 0;')
-			aps_print('LOC;')
-			exit()
-		elif(command.isdigit()):
-			setWatt(command)
+		if len(textIn) == 1:
+			if(command == 'on'):
+				print("Load turning on...")
+				myDevice.load_enable(True)
+			elif(command == 'off'):
+				print("Load turning off...")
+				myDevice.load_enable(False)
+			elif(command == 'readV'):
+				voltage = myDevice.get_value('V')
+				print('Voltage : {:.3f} (V)'.format(voltage))
+			elif(command == 'readI'):
+				current = myDevice.get_value('I')
+				print('Current: {:.3f} (A)'.format(current))
+			elif(command == 'readW'):
+				power = myDevice.get_value('W')
+				print('Current: {:.3f} (W)'.format(power))
+			elif(command == 'help'):
+				help()
+			elif(command == 'exit'):
+				print("Exiting...")
+				myDevice.load_enable(False)
+				break
+			else:
+				error_msg()
+		elif len(textIn) == 2:
+			value = float(textIn[1].strip(' '))
+			if command == 'watt':
+				myDevice.set_value('W', value)
+			elif command == 'amp':
+				tryValue = value
+				while True:
+					myDevice.set_value('I', tryValue)
+					sleep(1)
+					tmp = myDevice.get_value('I')
+					offset = tmp - value
+					if abs(offset) <= OFFSET_THRESHOLD:
+						break
+					tryValue -= offset
+				print('Settle to {:} (A)'.format(tmp))
 		else:
 			error_msg()
-	elif len(textIn) == 2:
-		# One-parameter commands
-		value = textIn[1].strip(' ')
-		if(command == 'amp'):
-			#if(int(value) < 4):
-			aps_print('CC:A ' + str(value) + ';')
-			#else:
-				#print('Maximum current is currently set to 4A')
-				#aps_print('CC:A 4;');
-		if(command == 'watt'):
-			setWatt(value)
-		if(command == 'inc'):
-			print("Increment value set to " + str(float(value)) + " W")
-			inc = float(value)
-	else:
-		# Multi-parameter commands
-		pass
-
-	aps_print('LOC;')
 
 
+if __name__=='__main__':
+	main()
 
