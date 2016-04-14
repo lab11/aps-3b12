baef9a7f4d69621e2759ed6b632e15a499a16ab8
Ye-Sheng Kuo <higherkuo@gmail.com>
2016-04-14 12:34:21 -0400

    add increment feature (follow Sam's script)

--- aps_3B12.py
+++ aps_3B12.py
@@ -5,68 +5,69 @@ from time import sleep
 DEV = '/dev/ttyUSB0'
 OFFSET_THRESHOLD = 0.003 # mA
 
-def error_msg():
-	print('Unrecognized command')
-	#print('Valid options are:')
-	#print('on - activate load')
-	#print('off - deactivate load')
-	#print('exit - quit application')
-	
-def help():
+def help(inc_val):
 	print("Commands:")
 	print("on\tTurn load on")
 	print("off\tTurn load off")
 	print("readV\tRead voltage value")
 	print("readI\tRead current value")
 	print("readW\tRead wattage value")
-	print("watt=XX\tSet load to XX W")
+	print("[watt=]XX\tSet load to XX W")
 	print("amp=XX\tSet load to XX A")
+	print("inc=XX\tSet increment to XX W")
+	print("<enter>\tIncrement wattage by {:} W".format(inc_val))
 	print("help\tDisplay help")
 	print("exit\tTurn load off and exit application\n")
 
 def main():
 	print("\nAPS 3B12 Control Script\n")
-	help()
+	input_watt = 0
+	inc_watt = 25
+	help(inc_watt)
 	myDevice = APS3B12.APS3B12(DEV)
-	#myDevice.get_loadState()
-	# For each command:
-	#	1. Set 3B12 to Remote
-	#	2. Perform operation
-	#	3. Set 3B12 to Local
 
 	while(1):
 		textIn = input('Enter input: ')
 		textIn = textIn.split('=')
 		command = textIn[0].strip(' ')
+		# no command, simply increment by inc_watt
+		if len(textIn[0]) == 0:
+			input_watt += inc_watt
+			#print('increment the load to {:} (W)'.format(input_watt))
+			myDevice.set_value('W', input_watt)
 		# No-parameter commands
-		if len(textIn) == 1:
-			if(command == 'on'):
+		elif len(textIn) == 1:
+			if command == 'on':
 				print("Load turning on...")
 				myDevice.load_enable(True)
-			elif(command == 'off'):
+			elif command == 'off':
 				print("Load turning off...")
 				myDevice.load_enable(False)
-			elif(command == 'readV'):
+			elif command == 'readV':
 				voltage = myDevice.get_value('V')
 				print('Voltage : {:.3f} (V)'.format(voltage))
-			elif(command == 'readI'):
+			elif command == 'readI':
 				current = myDevice.get_value('I')
 				print('Current: {:.3f} (A)'.format(current))
-			elif(command == 'readW'):
+			elif command == 'readW':
 				power = myDevice.get_value('W')
 				print('Current: {:.3f} (W)'.format(power))
-			elif(command == 'help'):
-				help()
-			elif(command == 'exit'):
+			elif command.isdigit():
+				input_watt = command
+				myDevice.set_value('W', input_watt)
+			elif command == 'help':
+				help(inc_watt)
+			elif command == 'exit':
 				print("Exiting...")
 				myDevice.load_enable(False)
 				break
 			else:
-				error_msg()
+				print('Unrecognized command')
 		elif len(textIn) == 2:
 			value = float(textIn[1].strip(' '))
 			if command == 'watt':
-				myDevice.set_value('W', value)
+				input_watt = value
+				myDevice.set_value('W', input_watt)
 			elif command == 'amp':
 				tryValue = value
 				while True:
@@ -78,8 +79,11 @@ def main():
 						break
 					tryValue -= offset
 				print('Settle to {:} (A)'.format(tmp))
+			elif command == 'inc':
+				inc_watt = value
+				print("Increment value set to {:} W".format(inc_watt))
 		else:
-			error_msg()
+			print('Unrecognized command')
 
 
 if __name__=='__main__':
