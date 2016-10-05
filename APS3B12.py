ae078f956d957f74aebef920fcabe4c14b836525
Ye-Sheng Kuo <higherkuo@gmail.com>
2016-10-05 18:49:07 -0400

    retab

--- APS3B12.py
+++ APS3B12.py
@@ -7,87 +7,87 @@ import sys
 
 class APS3B12(object):
 
-	def serial_write_byte_UTF8(self, text):
-		self.serial.write(text.encode(encoding="utf-8"))
-		time.sleep(.03)
+    def serial_write_byte_UTF8(self, text):
+        self.serial.write(text.encode(encoding="utf-8"))
+        time.sleep(.03)
 
-	def load_enable(self, enable):
-		# remote only mode
-		self.serial_write_byte_UTF8('REM;')
-		if enable:
-			print('Enabling load')
-			self.serial_write_byte_UTF8('LOAD 1;')
-			self.state = True
-		else:
-			print('Disabling load')
-			self.serial_write_byte_UTF8('CC:A 0;')
-			self.serial_write_byte_UTF8('LOAD 0;')
-			self.state = False
-		# local only mode
-		self.serial_write_byte_UTF8('LOC;')
+    def load_enable(self, enable):
+        # remote only mode
+        self.serial_write_byte_UTF8('REM;')
+        if enable:
+            print('Enabling load')
+            self.serial_write_byte_UTF8('LOAD 1;')
+            self.state = True
+        else:
+            print('Disabling load')
+            self.serial_write_byte_UTF8('CC:A 0;')
+            self.serial_write_byte_UTF8('LOAD 0;')
+            self.state = False
+        # local only mode
+        self.serial_write_byte_UTF8('LOC;')
 
-	def __init__(self, dev):
-		try:
-			self.serial = serial.Serial(dev, 9600)
-		except:
-			sys.exit('Cannot find {:}'.format(dev))
-		self.state = False
-		# remote only mode
-		self.serial_write_byte_UTF8('REM;')
-		# turn off load
-		self.load_enable(self.state)
-		# local only mode
-		self.serial_write_byte_UTF8('LOC;')
+    def __init__(self, dev):
+        try:
+            self.serial = serial.Serial(dev, 9600)
+        except:
+            sys.exit('Cannot find {:}'.format(dev))
+        self.state = False
+        # remote only mode
+        self.serial_write_byte_UTF8('REM;')
+        # turn off load
+        self.load_enable(self.state)
+        # local only mode
+        self.serial_write_byte_UTF8('LOC;')
 
-	def get_loadState(self):
-		# remote only mode
-		self.serial_write_byte_UTF8('REM;')
-		self.serial_write_byte_UTF8('LOAD?')
-		state = int(self.serial.read(1).decode(encoding='utf-8').rstrip())
-		print('Load state: {:}'.format(state))
-		# local only mode
-		self.serial_write_byte_UTF8('LOC;')
-		return True if state else False
-	
-	def get_value(self, measType):
-		# remote only mode
-		self.serial_write_byte_UTF8('REM;')
-		if measType == 'I':
-			self.serial_write_byte_UTF8('MEAS:CURR?;')
-		elif measType == 'V':
-			self.serial_write_byte_UTF8('MEAS:VOLT?;')
-		elif measType == 'W':
-			self.serial_write_byte_UTF8('MEAS:POW?;')
-		value = self.serial.read(8).decode(encoding='utf-8')
-		value = float(value.strip())
-		## local only mode
-		self.serial_write_byte_UTF8('LOC;')
-		return value
-	
-	def set_value(self, valType, value):
-		if not self.state:
-			print("Load not enabled, command ignored")
-			return -1
-		# remote only mode
-		self.serial_write_byte_UTF8('REM;')
-		value = float(value)
-		if valType == 'I':
-			self.serial_write_byte_UTF8('CC:A ' + str(value) + ';')
-			print('Setting current to {:.3f} (A)'.format(value))
-		elif valType == 'W':
-			print("Setting wattage to {:.3f} (W)".format(value))
-			print("Please wait for command to finish")
-			v_meas = self.get_value('V')
-			i = value/v_meas if v_meas > 0 else value/120
-			self.set_value('I', i)
-			time.sleep(1)
-			p_meas = self.get_value('W')
-			# v_meas <= 120, actual current should be higher than calcualted
-			if p_meas > 0:
-				i *= value/p_meas
-				self.set_value('I', i)
-		# local only mode
-		self.serial_write_byte_UTF8('LOC;')
-		time.sleep(1)
-		return self.get_value(valType)
+    def get_loadState(self):
+        # remote only mode
+        self.serial_write_byte_UTF8('REM;')
+        self.serial_write_byte_UTF8('LOAD?')
+        state = int(self.serial.read(1).decode(encoding='utf-8').rstrip())
+        print('Load state: {:}'.format(state))
+        # local only mode
+        self.serial_write_byte_UTF8('LOC;')
+        return True if state else False
+    
+    def get_value(self, measType):
+        # remote only mode
+        self.serial_write_byte_UTF8('REM;')
+        if measType == 'I':
+            self.serial_write_byte_UTF8('MEAS:CURR?;')
+        elif measType == 'V':
+            self.serial_write_byte_UTF8('MEAS:VOLT?;')
+        elif measType == 'W':
+            self.serial_write_byte_UTF8('MEAS:POW?;')
+        value = self.serial.read(8).decode(encoding='utf-8')
+        value = float(value.strip())
+        ## local only mode
+        self.serial_write_byte_UTF8('LOC;')
+        return value
+    
+    def set_value(self, valType, value):
+        if not self.state:
+            print("Load not enabled, command ignored")
+            return -1
+        # remote only mode
+        self.serial_write_byte_UTF8('REM;')
+        value = float(value)
+        if valType == 'I':
+            self.serial_write_byte_UTF8('CC:A ' + str(value) + ';')
+            print('Setting current to {:.3f} (A)'.format(value))
+        elif valType == 'W':
+            print("Setting wattage to {:.3f} (W)".format(value))
+            print("Please wait for command to finish")
+            v_meas = self.get_value('V')
+            i = value/v_meas if v_meas > 0 else value/120
+            self.set_value('I', i)
+            time.sleep(1)
+            p_meas = self.get_value('W')
+            # v_meas <= 120, actual current should be higher than calcualted
+            if p_meas > 0:
+                i *= value/p_meas
+                self.set_value('I', i)
+        # local only mode
+        self.serial_write_byte_UTF8('LOC;')
+        time.sleep(1)
+        return self.get_value(valType)
 
--- aps_3B12.py
+++ aps_3B12.py
@@ -7,97 +7,97 @@ CURRENT_OFFSET = 0.003 # mA
 WATT_OFFSET = 3 # W
 
 def help(inc_val):
-	print("Commands:")
-	print("on\tTurn load on")
-	print("off\tTurn load off")
-	print("readV\tRead voltage value")
-	print("readI\tRead current value")
-	print("readW\tRead wattage value")
-	print("[watt=]XX\tSet load to XX W")
-	print("amp=XX\tSet load to XX A")
-	print("inc=XX\tSet increment to XX W")
-	print("<enter>\tIncrement wattage by {:} W".format(inc_val))
-	print("help\tDisplay help")
-	print("exit\tTurn load off and exit application\n")
+    print("Commands:")
+    print("on\tTurn load on")
+    print("off\tTurn load off")
+    print("readV\tRead voltage value")
+    print("readI\tRead current value")
+    print("readW\tRead wattage value")
+    print("[watt=]XX\tSet load to XX W")
+    print("amp=XX\tSet load to XX A")
+    print("inc=XX\tSet increment to XX W")
+    print("<enter>\tIncrement wattage by {:} W".format(inc_val))
+    print("help\tDisplay help")
+    print("exit\tTurn load off and exit application\n")
 
 def main():
-	print("\nAPS 3B12 Control Script\n")
-	input_watt = 0
-	inc_watt = 25
-	help(inc_watt)
-	myDevice = APS3B12.APS3B12(DEV)
+    print("\nAPS 3B12 Control Script\n")
+    input_watt = 0
+    inc_watt = 25
+    help(inc_watt)
+    myDevice = APS3B12.APS3B12(DEV)
 
-	op1Dict = {'on':(myDevice.load_enable, True), \
-		      'off':(myDevice.load_enable, False), \
-			'readV':(myDevice.get_value, 'V'), \
-			'readI':(myDevice.get_value, 'I'), \
-			'readW':(myDevice.get_value, 'W'), \
-			 'help':(help, inc_watt),\
-			 'exit':(myDevice.load_enable, False)
-			  }
+    op1Dict = {'on':(myDevice.load_enable, True), \
+              'off':(myDevice.load_enable, False), \
+            'readV':(myDevice.get_value, 'V'), \
+            'readI':(myDevice.get_value, 'I'), \
+            'readW':(myDevice.get_value, 'W'), \
+             'help':(help, inc_watt),\
+             'exit':(myDevice.load_enable, False)
+              }
 
-	while(1):
-		textIn = input('Enter input: ')
-		textIn = textIn.split('=')
-		command = textIn[0].strip(' ')
-		# no command, simply increment by inc_watt
-		if len(textIn[0]) == 0:
-			input_watt += inc_watt
-			myDevice.set_value('W', input_watt)
-		# No-parameter commands
-		elif len(textIn) == 1:
-			if command == 'on':
-				print("Load turning on...")
-				myDevice.load_enable(True)
-			elif command == 'off':
-				print("Load turning off...")
-				myDevice.load_enable(False)
-			elif command == 'readV':
-				voltage = myDevice.get_value('V')
-				print('Voltage : {:.3f} (V)'.format(voltage))
-			elif command == 'readI':
-				current = myDevice.get_value('I')
-				print('Current: {:.3f} (A)'.format(current))
-			elif command == 'readW':
-				power = myDevice.get_value('W')
-				print('Current: {:.3f} (W)'.format(power))
-			elif command.isdigit():
-				input_watt = command
-				myDevice.set_value('W', input_watt)
-			elif command == 'help':
-				help(inc_watt)
-			elif command == 'exit':
-				print("Exiting...")
-				myDevice.load_enable(False)
-				break
-			else:
-				print('Unrecognized command')
-		elif len(textIn) == 2:
-			value = float(textIn[1].strip(' '))
-			if command == 'watt' or command == 'amp':
-				if command == 'watt':
-					input_watt = value
-				tmpType = 'W' if command == 'watt' else 'I'
-				tmpOffset = WATT_OFFSET if command == 'watt' else CURRENT_OFFSET
-				tryValue = value
-				# recursively correct the setting
-				while True:
-					tmp = myDevice.set_value(tmpType, tryValue)
-					if tmp < 0:
-						break
-					offset = tmp - value
-					if abs(offset) <= tmpOffset:
-						break
-					tryValue -= offset
-				if tmp > 0:
-					print('Settle to {:} {:}'.format(tmp, tmpType))
-			elif command == 'inc':
-				inc_watt = value
-				print("Increment value set to {:} W".format(inc_watt))
-		else:
-			print('Unrecognized command')
+    while(1):
+        textIn = input('Enter input: ')
+        textIn = textIn.split('=')
+        command = textIn[0].strip(' ')
+        # no command, simply increment by inc_watt
+        if len(textIn[0]) == 0:
+            input_watt += inc_watt
+            myDevice.set_value('W', input_watt)
+        # No-parameter commands
+        elif len(textIn) == 1:
+            if command == 'on':
+                print("Load turning on...")
+                myDevice.load_enable(True)
+            elif command == 'off':
+                print("Load turning off...")
+                myDevice.load_enable(False)
+            elif command == 'readV':
+                voltage = myDevice.get_value('V')
+                print('Voltage : {:.3f} (V)'.format(voltage))
+            elif command == 'readI':
+                current = myDevice.get_value('I')
+                print('Current: {:.3f} (A)'.format(current))
+            elif command == 'readW':
+                power = myDevice.get_value('W')
+                print('Current: {:.3f} (W)'.format(power))
+            elif command.isdigit():
+                input_watt = command
+                myDevice.set_value('W', input_watt)
+            elif command == 'help':
+                help(inc_watt)
+            elif command == 'exit':
+                print("Exiting...")
+                myDevice.load_enable(False)
+                break
+            else:
+                print('Unrecognized command')
+        elif len(textIn) == 2:
+            value = float(textIn[1].strip(' '))
+            if command == 'watt' or command == 'amp':
+                if command == 'watt':
+                    input_watt = value
+                tmpType = 'W' if command == 'watt' else 'I'
+                tmpOffset = WATT_OFFSET if command == 'watt' else CURRENT_OFFSET
+                tryValue = value
+                # recursively correct the setting
+                while True:
+                    tmp = myDevice.set_value(tmpType, tryValue)
+                    if tmp < 0:
+                        break
+                    offset = tmp - value
+                    if abs(offset) <= tmpOffset:
+                        break
+                    tryValue -= offset
+                if tmp > 0:
+                    print('Settle to {:} {:}'.format(tmp, tmpType))
+            elif command == 'inc':
+                inc_watt = value
+                print("Increment value set to {:} W".format(inc_watt))
+        else:
+            print('Unrecognized command')
 
 
 if __name__=='__main__':
-	main()
+    main()
 
