5c3cf033b45b4c8385b0f689a27f7a1d0f8d43c9
Ye-Sheng Kuo <higherkuo@gmail.com>
2017-02-02 12:43:49 -0500

    add bank / wave set / get support

--- tcpServer.py
+++ tcpServer.py
@@ -11,7 +11,7 @@ WATT_OFFSET = 3 # W
 
 HOST = 'lab11power.ddns.net'
 PORT = 4908
-myDevice = APS3B12.APS3B12(DEV)
+myDevice = APS3B12.APS3B12(DEV, 1)
 
 def main():
     if sys.version_info[0] < 3:
@@ -65,15 +65,22 @@ def cmdExec(myStr):
             'readV':(myDevice.get_value, 'V'), \
             'readI':(myDevice.get_value, 'I'), \
             'readW':(myDevice.get_value, 'W'), \
+            'readBank':(myDevice.set_get_bank_wave, 'GET'), \
+            'readWave':(myDevice.set_get_bank_wave, 'GET'), \
              'exit':(myDevice.load_enable, False)
               }
 
     if myCmd in op1Dict:
-        (myFuc, myArg) = op1Dict.get(myCmd)
-        res = myFuc(myArg)
-        if res:
-            return res
-        return 'Execute succeed'
+        if myCmd == 'readBank' or myCmd == 'readWave':
+            tmp = 'BANK' if myCmd == 'readBank' else 'WAVE'
+            res = myDevice.set_get_bank_wave('GET', tmp, 0)
+            return str(res) + '\r\n'
+        else:
+            (myFuc, myArg) = op1Dict.get(myCmd)
+            res = myFuc(myArg)
+            if res:
+                return res
+        return 'Execute succeed\r\n'
     elif '=' in myCmd and len(myCmd.split('='))==2:
         myCmd = myCmd.split('=')
         if (myCmd[0] == 'watt' or myCmd[0] == 'amp') and isFloat(myCmd[1]):
@@ -94,7 +101,12 @@ def cmdExec(myStr):
             if tmp > 0:
                 print('Settle to {:} {:}'.format(tmp, tmpType))
                 return tmp
-    return 'Invalid Command'
+        elif (myCmd[0] == 'bank' or myCmd[0] == 'wave') and isFloat(myCmd[1]):
+            myDevice.set_get_bank_wave('SET', myCmd[0].upper(), int(myCmd[1]))
+            tmp = myDevice.set_get_bank_wave('GET', myCmd[0].upper(), 0)
+            print('Set ' + myCmd[0].upper() + ' to ' + str(tmp))
+            return str(tmp) + '\r\n'
+    return 'Invalid Command\r\n'
     
 
 if __name__=="__main__":
